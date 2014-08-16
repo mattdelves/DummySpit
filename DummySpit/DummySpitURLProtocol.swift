71cbd54523267cf14ee2e831c1546962051898a3
Matthew Delves <matt@reformedsoftware.com>
2014-08-16 15:49:37 +1000

    Start of dummy spit

---
+++ DummySpit/DummySpitURLProtocol.swift
@@ -0,0 +1,125 @@
+//
+//  DummySpitURLProtocol.swift
+//  DummySpit
+//
+//  Created by Matthew Delves on 16/08/2014.
+//  Copyright (c) 2014 Matthew Delves. All rights reserved.
+//
+
+//
+//  Initial work by DBlechoc
+//
+
+import Foundation
+
+public struct DummySpitServiceResponse
+{
+  public let body: AnyObject
+  public let header: NSDictionary
+  public let urlComponent: String?
+  public let statusCode: Int
+  public let error: NSError?
+  
+  public init (body: AnyObject, header: NSDictionary, urlComponentToMatch urlComponent: String? = nil, statusCode: Int = 200, error: NSError? = nil)
+  {
+    self.body = body
+    self.header = header
+    self.urlComponent = urlComponent
+    self.statusCode = statusCode
+    self.error = error
+  }
+}
+
+var storage: [DummySpitServiceResponse]?
+
+public class DummySpitURLProtocol: NSURLProtocol
+{
+  public class func cannedResponses(responses: [DummySpitServiceResponse]?)
+  {
+    storage = responses
+  }
+  
+  public class func cannedResponse(response: DummySpitServiceResponse?)
+  {
+    if let aResponse = response
+    {
+      storage = [aResponse]
+    }
+    else
+    {
+      storage = nil
+    }
+  }
+  
+  public class func responsesForURL(URL: NSURL) -> [DummySpitServiceResponse]?
+  {
+    if let tmpResponses = storage
+    {
+      var responses = (tmpResponses as [DummySpitServiceResponse]).filter{
+        cannedResponse -> Bool in
+        
+        return (cannedResponse.urlComponent.hasValue) ? contains(URL.pathComponents as [String], cannedResponse.urlComponent!) : true
+      }
+      
+      // give priority to responses that have a urlComponent
+      responses.sort {
+        (response1: DummySpitServiceResponse, response2: DummySpitServiceResponse) -> Bool in
+        return response1.urlComponent > response1.urlComponent
+      }
+      
+      return responses
+    }
+    
+    return nil
+  }
+  
+  override public class func canInitWithRequest(request: NSURLRequest!) -> Bool
+  {
+    let schemeIsMock = request.URL.scheme == "mock"
+    var urlMatched = responsesForURL(request.URL)?.count > 0
+    
+    return (schemeIsMock && urlMatched)
+  }
+  
+  override public class func canonicalRequestForRequest(request: NSURLRequest!) -> NSURLRequest!
+  {
+    return request
+  }
+  
+  override public class func requestIsCacheEquivalent(a: NSURLRequest!, toRequest b: NSURLRequest!) -> Bool
+  {
+    return a.URL == b.URL
+  }
+  
+  override public func startLoading()
+  {
+    let request = self.request
+    let client: NSURLProtocolClient = self.client
+    
+    if let cannedResponses = self.dynamicType.responsesForURL(request.URL)
+    {
+      let cannedResponse: DummySpitServiceResponse = cannedResponses[0]
+      if let error = cannedResponse.error
+      {
+        client.URLProtocol(self, didFailWithError: error)
+      }
+      else
+      {
+        let response = NSHTTPURLResponse(URL: request.URL, statusCode: cannedResponse.statusCode, HTTPVersion: "HTTP/1.1", headerFields: cannedResponse.header)
+        client.URLProtocol(self, didReceiveResponse: response, cacheStoragePolicy: NSURLCacheStoragePolicy.NotAllowed)
+        
+        let jsonData = NSJSONSerialization.dataWithJSONObject(cannedResponse.body, options: NSJSONWritingOptions(), error: nil)
+        client.URLProtocol(self, didLoadData: jsonData)
+        
+        client.URLProtocolDidFinishLoading(self)
+      }
+      
+    }
+    
+  }
+  
+  override public func stopLoading()
+  {
+    
+  }
+}
--- DummySpitTests/DummySpitTests.swift
+++ DummySpitTests/DummySpitTests.swift
@@ -6,31 +6,112 @@
 //  Copyright (c) 2014 Matthew Delves. All rights reserved.
 //
 
-import UIKit
-import XCTest
+//
+//  Based upon the work of Dblechoc.
+//
 
-class DummySpitTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+import Quick
+import Nimble
+import DummySpit
+
+class DummySpitURLProtocolSpec: QuickSpec {
+  override func spec() {
+    describe("DummySpitServiceResponse") {
+      var response: MockWebServiceResponse!
+      var error: NSError!
+      
+      describe("Init") {
+        beforeEach {
+          error = NSError(domain: "au.com.test.domain", code: 150, userInfo: nil)
+          response = DummySpitServiceResponse(body: ["test": "blah"], header: ["Content-Type": "application/json; charset=utf-8"], statusCode: 404, error: error)
         }
+        
+        it("should have a body") {
+          expect(response.body as? NSDictionary).to(equal(["test": "blah"]))
+        }
+        
+        it("should have an header") {
+          expect(response.header).to(equal(["Content-Type": "application/json; charset=utf-8"]))
+        }
+        
+        it("should have a statusCode") {
+          expect(response.statusCode).to(equal(404))
+        }
+        
+        it("should have an error") {
+          expect(response.error).to(equal(error))
+        }
+      }
     }
     
+    describe("DummySpitURLProtocol") {
+      beforeEach {
+        let response = DummySpitServiceResponse(body: ["test": "blah"], header: ["Content-Type": "application/json; charset=utf-8"])
+        DummySpitURLProtocol.cannedResponse(response)
+      }
+      
+      afterEach {
+        DummySpitURLProtocol.cannedResponse(nil)
+      }
+      
+      
+      describe("canInitWithRequest") {
+        context("without a url component to match") {
+          context("when using mock scheme") {
+            it("should be able to init a request") {
+              let request = NSURLRequest(URL: NSURL(string: "mock://www.apple.com"))
+              expect(DummySpitURLProtocol.canInitWithRequest(request)).to(beTruthy())
+            }
+          }
+          
+          context("when using http scheme") {
+            it("should not be able to init a request") {
+              let request = NSURLRequest(URL: NSURL(string: "http://www.apple.com"))
+              expect(DummySpitURLProtocol.canInitWithRequest(request)).to(beFalsy())
+            }
+          }
+        }
+        
+        context("with a url component to match") {
+          beforeEach {
+            let response = DummySpitServiceResponse(body: ["test": "blah"], header: ["Content-Type": "application/json; charset=utf-8"], urlComponentToMatch:"GetStopsByRouteAndDirection.ashx")
+            let responseNoComponent = DummySpitServiceResponse(body: ["test2": "blah"], header: ["Content-Type": "application/json; charset=utf-8"])
+            DummySpitURLProtocol.cannedResponses([response, responseNoComponent])
+          }
+          
+          it("should be able to init a request") {
+            let request = NSURLRequest(URL: NSURL(string: "mock://www.apple.com/Controllers/GetStopsByRouteAndDirection.ashx?r=123&u=true"))
+            expect(DummySpitURLProtocol.canInitWithRequest(request)).to(beTruthy())
+          }
+        }
+      }
+      
+      describe("canonicalRequestForRequest") {
+        it("should return the same request") {
+          let request = NSURLRequest(URL: NSURL(string: "mock://www.apple.com"))
+          expect(DummySpitURLProtocol.canonicalRequestForRequest(request)).to(equal(request))
+        }
+      }
+      
+      describe("requestIsCacheEquivalent") {
+        context("when given requests are equal") {
+          it("should be equivalent with the same request is given twice") {
+            let requestA = NSURLRequest(URL: NSURL(string: "mock://www.apple.com"))
+            let requestB = NSURLRequest(URL: NSURL(string: "mock://www.apple.com"))
+            expect(DummySpitURLProtocol.requestIsCacheEquivalent(requestA, toRequest: requestB )).to(beTruthy())
+          }
+        }
+        
+        context("when given requests aren't equal") {
+          it("should be equivalent with the same request is given twice") {
+            let requestA = NSURLRequest(URL: NSURL(string: "mock://www.apple.com"))
+            let requestB = NSURLRequest(URL: NSURL(string: "http://www.apple.com"))
+            expect(DummySpitURLProtocol.requestIsCacheEquivalent(requestA, toRequest: requestB )).to(beFalsy())
+          }
+        }
+      }
+      
+      // startLoading is tested inside NetworkServiceSpec
+    }
+  }
 }
