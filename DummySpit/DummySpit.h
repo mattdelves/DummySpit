ec7d8976de4d673f7e174bc4d191f57596d3917e
Matthew Delves <matt@reformedsoftware.com>
2014-08-16 15:35:07 +1000

    Initial Commit

---
+++ DummySpit/DummySpit.h
@@ -0,0 +1,19 @@
+//
+//  DummySpit.h
+//  DummySpit
+//
+//  Created by Matthew Delves on 16/08/2014.
+//  Copyright (c) 2014 Matthew Delves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DummySpit.
+FOUNDATION_EXPORT double DummySpitVersionNumber;
+
+//! Project version string for DummySpit.
+FOUNDATION_EXPORT const unsigned char DummySpitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DummySpit/PublicHeader.h>
+
+
