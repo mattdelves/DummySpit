11ecccb74deb193176871ab44c1f8282bdaf6c4b
Matthew Delves <matt@reformedsoftware.com>
2014-11-22 20:22:11 +1100

    update build script

--- build_and_test.sh
+++ build_and_test.sh
@@ -31,5 +31,5 @@ git checkout -qf "$BUILDBOX_COMMIT"
 git submodule init
 git submodule update
 
-xcodebuild -workspace DummySpit.xcworkspace -scheme DummySpit -sdk iphonesimulator8.1 -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.1' clean build test | xcpretty
+xcodebuild -workspace DummySpit.xcworkspace -scheme DummySpit -sdk iphonesimulator8.2 -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.2' clean build test | xcpretty
 
