ce8e359b03b7afdbb0504144f490a982c5ed1d81
Matt Delves <matt.delves@outware.com.au>
2015-05-06 19:04:26 +1000

    Introduce OSX target

---
+++ DummySpit-OSX/DummySpit-OSX.h
@@ -0,0 +1,19 @@
+//
+//  DummySpit-OSX.h
+//  DummySpit-OSX
+//
+//  Created by Matt Delves on 6/05/2015.
+//  Copyright (c) 2015 Matthew Delves. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DummySpit-OSX.
+FOUNDATION_EXPORT double DummySpit_OSXVersionNumber;
+
+//! Project version string for DummySpit-OSX.
+FOUNDATION_EXPORT const unsigned char DummySpit_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DummySpit_OSX/PublicHeader.h>
+
+
